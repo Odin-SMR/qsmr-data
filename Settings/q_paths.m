0b965ef9c40a7ea34be0820b31ef07c563ca0769
Francesco Grieco <francesco@coral.rss.chalmers.se>
2019-02-27 16:43:29 +0100

    2019-02-27  Francesco Grieco  <francesco.grieco@chalmers.se>
    	* Settings/q_paths.m: Q.FOLDER_BACKEND back to 'Backend'.

--- Settings/q_paths.m
+++ Settings/q_paths.m
@@ -26,8 +26,8 @@ investr(2:end)       = lower( investr(2:end) );
 Q.FOLDER_ANTENNA     = fullfile( topfolder, 'DataPrecalced', 'Antenna' );  
 Q.FOLDER_BDX         = fullfile( topfolder, 'DataPrecalced', 'SpeciesApriori', ...
                                  'Bdx' );
-%Q.FOLDER_BACKEND     = fullfile( topfolder, 'DataPrecalced', 'Backend' );
-Q.FOLDER_BACKEND     = fullfile( topfolder, 'DataPrecalced', 'Backend_PLLdisf' );
+Q.FOLDER_BACKEND     = fullfile( topfolder, 'DataPrecalced', 'Backend' );
+%Q.FOLDER_BACKEND     = fullfile( topfolder, 'DataPrecalced', 'Backend_PLLdisf' );
 Q.FOLDER_FGRID       = fullfile( topfolder, 'DataPrecalced', 'Fgrid', investr );  
 Q.FOLDER_MSIS90      = fullfile( topfolder, 'DataInput', 'Temperature' );  
 
